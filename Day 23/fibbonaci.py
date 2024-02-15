39819026d6da488fe7a5ec872df06c3a3f006943
juliasta702@gmail.com <juliasta702@gmail.com>
2024-02-15 17:59:20 +0800

    Day 23

---
+++ Day 23/fibbonaci.py
@@ -0,0 +1,9 @@
+def fibonaci(n):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        next_fib = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_fib)
+
+    return fib_sequence[:n]
+
+print(fibonaci(10))
