8e7775bcf9ba649cd99033aab30c75eed12edcd4
juliasta702@gmail.com <juliasta702@gmail.com>
2024-03-07 19:24:40 +0800

    Day 44

---
+++ Day 44/FindMaximumandMinimumValuesofaList.py
@@ -0,0 +1,10 @@
+def maxNum(numList):
+    return max(numList)
+
+def minNum(numList):
+    return min(numList)
+
+numList = [56, 50, -100, 0, 9, 11]
+print(maxNum(numList))
+print(minNum(numList))
+
