da84edf51f26bffb9412f0773f03df982b752ab4
juliasta702@gmail.com <juliasta702@gmail.com>
2024-06-18 19:27:49 +0800

    Day 70

---
+++ Day 70/opposite-number.py
@@ -0,0 +1,12 @@
+""" 
+Given number, find the opposite of the number
+example
+input: 2
+output: -2
+
+"""
+def opposite(number: int | float) -> int | float:
+    return number - number - number 
+
+print(opposite(-15))
+
