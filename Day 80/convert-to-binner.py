acdf08ebb5db311784a893a281cd57e5112de4cf
juliasta702@gmail.com <juliasta702@gmail.com>
2024-07-22 13:37:57 +0800

    Day 80

---
+++ Day 80/convert-to-binner.py
@@ -0,0 +1,14 @@
+def convert_to_binner(n):
+    return f"{n:o}"
+
+print(convert_to_binner(12))
+
+
+""" 
+n:b artinya mengkonversi nilai n menjadi sebuah biner
+untuk hexadeximal menggukan x, dan X
+x lowercase
+X uppercase
+
+untuk octal menggunakan o, jadinya n:o
+"""
---
+++ Day 80/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(convert_to_binner(2))
